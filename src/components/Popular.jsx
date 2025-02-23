4a8279c44279b54aacddff34c484089af81c1b4f
Rakeshgowda <130374347+rakeshgowda123@users.noreply.github.com>
2025-02-23 21:26:03 +0530

    Update Popular.jsx

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -22,18 +22,11 @@ function Popular() {
   }, []);
 
   const getPopular = async () => {
-    // const check = localStorage.getItem("popular");
-
-    if (check) {
-      setPopular(JSON.parse(check));
-    } else {
-      const api = await fetch(
-        `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
-      );
-      const data = await api.json();
-      // localStorage.setItem("popular", JSON.stringify(data.recipes));
-      setPopular(data.recipes || []);
-    }
+    const api = await fetch(
+      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+    );
+    const data = await api.json();
+    setPopular(data.recipes || []);
   };
 
   // Render mobile layout
